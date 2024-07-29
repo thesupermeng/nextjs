8c515f919ac725f029ffa12bf1e8983d1966f84e
supermeng <supermeng@redfin.tech>
2024-07-29 13:34:25 +0800

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 import path from 'path';
-import { i18n } from './next-i18next.config.js';
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -29,7 +29,7 @@ const nextConfig = {
       },
     ];
   },
-  i18n,
+
   transpilePackages: ['crypto-js'],
   logging: {
     fetches: {
