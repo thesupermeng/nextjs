fedd593454bfc4f41a767d83b008a086c03bd8b0
supermeng <supermeng@redfin.tech>
2024-07-29 13:45:22 +0800

    fix 18n

--- app/i18n.js
+++ app/i18n.js
@@ -7,7 +7,7 @@ import { initReactI18next } from 'react-i18next';
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
-    translation: require('../../public/locales/en/translation.json'),
+    translation: require('../public/locales/en/translation.json'),
   },
 };
 
