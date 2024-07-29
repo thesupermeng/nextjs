e016e403dc5c96f0e520aacffd659dcae3e8ad5e
supermeng <supermeng@redfin.tech>
2024-07-29 11:10:10 +0800

    try

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(process.cwd(), 'src');
+    return config;
+  },
+  // Other configurations can be added here
+};
 
 export default nextConfig;
